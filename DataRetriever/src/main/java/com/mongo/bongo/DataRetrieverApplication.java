253782fba8b530b2ae24662342f9101d3b10b368
Yadav <varun.yadav@siemens.com>
2020-03-09 18:41:18 +0530

    Nue prject

---
+++ DataRetriever/src/main/java/com/mongo/bongo/DataRetrieverApplication.java
@@ -0,0 +1,13 @@
+package com.mongo.bongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataRetrieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataRetrieverApplication.class, args);
+	}
+
+}
---
+++ DataRetriever/src/test/java/com/mongo/bongo/DataRetrieverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mongo.bongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataRetrieverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
